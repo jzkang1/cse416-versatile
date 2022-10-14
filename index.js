a7f123158a57a3ef4a0c2335e44cb3c91d1c5603
Jerry <jerry.kang@stonybrook.edu>
2022-10-14 14:03:56 -0400

    Added frontend serve

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require('path')
 
 const app = express();
 
@@ -24,12 +25,19 @@ app.get("/api/hello/", cors(), async (req, res, next) => {
     } catch (err) {
         next(err);
     }
-})
+});
 
-let x = 5;
-let y = "${x}"
 // Choose the port and start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Started express app on port ${PORT}`);
 })
+
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/frontend/build/index.html'))
+});
