dbee2568768d7f07d893314e86d1d99288daf8cc
huang-daniel <daniel.pinyao.huang@gmail.com>
2022-10-29 22:21:32 -0400

    Versatile logo

--- frontend/src/components/HomeScreen.js
+++ frontend/src/components/HomeScreen.js
@@ -2,8 +2,13 @@ import Box from '@mui/material/Box';
 
 export default function HomeScreen() {
     return (
+        
         <Box sx={{ flexGrow: 1 }}>
             HOME SCREEN
+            <div>
+                <img src={require('../images/versatileLogo.png')} alt="Image" />
+            </div>
         </Box>
+        
     )
 }
