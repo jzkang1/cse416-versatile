d36e7dc34eaebaf03e324510472277dc828f69bc
huang-daniel <daniel.pinyao.huang@gmail.com>
2022-10-29 21:43:05 -0400

    Hello world HomeScreen.js

---
+++ frontend/src/components/HomeScreen.js
@@ -0,0 +1,16 @@
+import React, { useContext, useEffect } from 'react'
+import { GlobalStoreContext } from '../store'
+import ListCard from './ListCard.js'
+import { Fab, Typography } from '@mui/material'
+import AddIcon from '@mui/icons-material/Add';
+import List from '@mui/material/List';
+
+const HomeScreen = () => {
+    return (
+        <div>
+            Hello World
+        </div>
+    )
+}
+
+export default HomeScreen;
