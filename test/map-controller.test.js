babdc6da42dc3d8fb45b5c21021af6dcfcd2be5c
Sungjun <sungjun@sj-MacBook.local>
2022-10-30 21:00:23 -0400

    updated env dir

--- test/map-controller.test.js
+++ test/map-controller.test.js
@@ -1,7 +1,7 @@
 const map = require("../controllers/map-controller.js");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-dotenv.config({ path: "./.env" });
+dotenv.config();
 
 beforeAll(() => {
   mongoose.disconnect();
